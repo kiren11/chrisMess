27e62522b5bb09f53130221f914bbfc8aad5fec7
Kiren Kaur <kaurkir@umail.iu.edu>
2018-06-26 11:17:07 -0400

    Bonus 1 - button changes the second heading

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const button = document.querySelector('button')
 //create function to update header text when button is clicked
 const updateText = function() {
 
-  const headingChanger = document.querySelector("h1")
-  headingChanger.textContent = 'Chrismess is being fixed!';
+  //second heading is selected
+  const headingChanger = document.querySelector("#sTitle")
+
+  //change the second heading to this text
+  headingChanger.textContent = 'Chrismess is being fixed!'
 }
 
 button.addEventListener('click', updateText)
