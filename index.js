9e21199b3c1deddede5d0be295517d937d0f06c6
Kiren Kaur <kaurkir@umail.iu.edu>
2018-06-26 11:15:22 -0400

    Homework - button changes heading

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("it works");
+// js file for chrismess
+
+//select button element
+const button = document.querySelector('button')
+
+//create function to update header text when button is clicked
+const updateText = function() {
+
+  const headingChanger = document.querySelector("h1")
+  headingChanger.textContent = 'Chrismess is being fixed!';
+}
+
+button.addEventListener('click', updateText)
