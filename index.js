3dcd4947f25e0804aca062dd64a2ba59d7bfafb7
Kiren Kaur <kaurkir@umail.iu.edu>
2018-06-26 12:43:25 -0400

    Bonus 2 + 3 - (Added form and ability to press enter)

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 // js file for chrismess
 
 //select button element
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 
 //create function to update header text when button is clicked
-const updateText = function() {
+const updateText = function(e) {
 
-  //second heading is selected
-  const headingChanger = document.querySelector("#sTitle")
+  e.preventDefault();
+
+  //get input
+  const input = document.querySelector('#inputC');
+
+  //get heading
+  const heading = document.getElementById('fTitle');
 
   //change the second heading to this text
-  headingChanger.textContent = 'Chrismess is being fixed!'
+  heading.textContent = input.value;
 }
 
-button.addEventListener('click', updateText)
+//submit form if 'enter' button is pressed
+document.querySelector("#inputC").addEventListener("keyup", function(event) {
+
+    event.preventDefault();
+    if(event.keyCode === "13") {
+        button.click();
+    }   
+
+});
+
+button.addEventListener('click', updateText);
