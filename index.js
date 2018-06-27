68809a688f98035ebb6480070e0cd9882eab4c8a
Kiren Kaur <kaurkir@umail.iu.edu>
2018-06-27 13:56:32 -0400

    Homework - each movie gets added to an array

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//array that will store movies
+const flickArr = [];
+
 //select the form
 const form = document.querySelector('form#flickForm')
 
 //add first field to the list
 const appendToList = function(ev) {
+
   ev.preventDefault()
 
   //target is form
@@ -17,23 +21,32 @@ const appendToList = function(ev) {
   const fList = document.querySelector('#movieNames')
   fList.appendChild(firstField)
 
+  flickArr.push(flickName)
+
+  console.log(flickArr)
+  
+
 }
 
 const appendToList2 = function(ev) {
-    ev.preventDefault()
-  
-    //target is form
-    const f = ev.target
+
+  ev.preventDefault()
+
+  //target is form
+  const f = ev.target
 
   //add second value to list
   const yearName = f.yearName.value
-
 
   const secField = document.createElement('li')
   secField.textContent = yearName
   const sList = document.querySelector('#yearName')
   sList.appendChild(secField)
 
+  flickArr.push(yearName)
+
+  console.log(flickArr)
+  
   f.reset()
 }
 
