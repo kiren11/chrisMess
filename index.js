2233887267b4ac9fc9c5f8ebd00615e30a1c9153
Kiren Kaur <kaurkir@umail.iu.edu>
2018-06-28 15:47:51 -0400

    Start of Day 4

--- index.js
+++ index.js
@@ -1,55 +1,89 @@
-//array that will store movies
-const flickArr = [];
+class App {
+  constructor() {
+    this.flicks = []
+    this.list = document.querySelector('#flicks')
 
-//select the form
-const form = document.querySelector('form#flickForm')
+    const form = document.querySelector('form#flickForm')
+    form.addEventListener('submit', (ev) => {
+      ev.preventDefault()
+      this.handleSubmit(ev)
+    })
+  }
 
-//add first field to the list
-const appendToList = function(ev) {
+  renderProperty(name, value) {
+    const span = document.createElement('span')
+    span.classList.add(name)
+    span.textContent = value
+    return span
+  }
 
-  ev.preventDefault()
+  renderItem(flick) {
+    const item = document.createElement('li')
+    item.classList.add('flick')
 
-  //target is form
-  const f = ev.target
+    // get the list of properties
+    const properties = Object.keys(flick)
 
-  //name of movie is the first field
-  const flickName = f.flickName.value
+    // loop over the properties
+    properties.forEach((propertyName) => {
+      // build a span, and append it to the list
+      const span = this.renderProperty(propertyName, flick[propertyName])
+      item.appendChild(span)
+    })
 
-  //append movie name to list
-  const firstField = document.createElement('li')
-  firstField.textContent = flickName
-  const fList = document.querySelector('#movieNames')
-  fList.appendChild(firstField)
+    // add a delete button
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'delete'
+    deleteButton
+      .addEventListener(
+        'click',
+        (_ev) => this.removeFlick(flick, item)
+      )
+    item.appendChild(deleteButton)
 
-  flickArr.push(flickName)
+    // add a favorite button
+    const favButton = document.createElement('button')
+    favButton.textContent = 'favorite'
+    favButton
+      .addEventListener('click',
+        (_ev) => this.toggleFavorite(flick, item)
+      )
+    item.appendChild(favButton)
 
-  console.log(flickArr)
-  
+    return item
+  }
 
+  toggleFavorite(flick, item) {
+    // update both the UI and the array
+    flick.favorite = item.classList.toggle('fav')
+  }
+
+  removeFlick(flick, item) {
+    // remove from the UI
+    this.list.removeChild(item)
+
+    // remove from the array
+    const i = this.flicks.indexOf(flick)
+    this.flicks.splice(i, 1)
+  }
+
+  handleSubmit(ev) {
+    const f = ev.target
+
+    const flick = {
+      name: f.flickName.value,
+      chris: f.chrisName.value,
+      favorite: false,
+    }
+
+    this.flicks.push(flick)
+
+    const item = this.renderItem(flick)
+    this.list.appendChild(item)
+
+    f.reset()
+    f.flickName.focus()
+  }
 }
 
-const appendToList2 = function(ev) {
-
-  ev.preventDefault()
-
-  //target is form
-  const f = ev.target
-
-  //add second value to list
-  const yearName = f.yearName.value
-
-  const secField = document.createElement('li')
-  secField.textContent = yearName
-  const sList = document.querySelector('#yearName')
-  sList.appendChild(secField)
-
-  flickArr.push(yearName)
-
-  console.log(flickArr)
-  
-  f.reset()
-}
-
-//add the second field to the list
-form.addEventListener('submit', appendToList)
-form.addEventListener('submit', appendToList2)
+const app = new App()
